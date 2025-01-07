b19af2092f65b7fa48b38961fe31857039b0304b
Gabriel Araujo <pro.gabrielteixeira@gmail.com>
2025-01-07 11:58:14 +0000

    Order, Instant, ISO 8601

---
+++ src/main/java/com/study/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.study.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.study.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+}
--- src/main/java/com/study/course/repositories/UserRepository.java
+++ src/main/java/com/study/course/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.study.course.entities.User;
 
-public interface UserRepositories extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
